60bd98794c5acfb6aa4c77d3d925b724ed3f1946
Alvin <alvingeorge>
2021-01-06 18:42:33 +0530

    initialize.sh

---
+++ initialize.sh
@@ -0,0 +1,17 @@
+wget https://github.com/ARMmbed/mbedtls/archive/v2.24.0.tar.gz
+tar -zxvf v2.24.0.tar.gz
+cd mbedtls-2.24.0/
+make test
+
+# echo "----------------------------------------------"
+# echo "things to try"
+# echo ls -l tests/test_suite_ssl*
+# echo tests/test_suite_ssl 
+# echo cd tests/
+# echo ./test_suite_ssl 
+# echo cd ..
+# echo vim tests/test_suite_ssl.c 
+# echo ag mbedtls_ssl_read
+# echo ag mbedtls_ssl_conf_cid
+# echo ag mbedtls_ssl_get_peer_cid
+
