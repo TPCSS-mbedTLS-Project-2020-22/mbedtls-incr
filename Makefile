093057b7ab858abffdf5e7b75bdfa5ae1fd52f2f
Alvin <alvingeorge>
2021-01-21 20:16:41 +0530

    Makefile, lib.rs

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
 test:
-	cd mtlsrust/; cargo build; cd -
-	cd mbedtls-2.24.0/; make test; cd -
+	cd mtlsrust/; cargo build
+	cd mbedtls-2.24.0/; make test
 
--- mtlsrust/src/lib.rs
+++ mtlsrust/src/lib.rs
@@ -1,3 +1,13 @@
+
+#[no_mangle]
+// pub unsafe extern "C" fn func_in_rust(x: i32) -> i32 {
+pub extern "C" fn func_in_rust(x: i32) -> i32 {
+        return x + 10;
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
