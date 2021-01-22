4d5e1db33785c75da894569bd905af9ce6deebd9
Alvin <alvingeorge>
2021-01-22 11:26:06 +0530

    port pkcs12_fill_buffer()  from library/pkcs12.c  to pkcs12.rs  -- all tests PASS

--- mtlsrust/src/pkcs12.rs
+++ mtlsrust/src/pkcs12.rs
@@ -1,5 +1,6 @@
 extern crate libc;
-use libc::{size_t, c_uchar};
+use libc::{size_t, c_uchar, c_void};
+use libc::{memcpy};
 
 
 /*
@@ -10,14 +11,20 @@ static void pkcs12_fill_buffer( unsigned char *data, size_t data_len,
                                 const unsigned char *filler, size_t fill_len )
 */
 #[no_mangle]
-pub extern "C" fn pkcs12_fill_buffer(
-    _data: *mut c_uchar,
-    _data_len: size_t,
-    _filler: *const c_uchar,
-    _fill_len: size_t) {
+pub extern "C" fn pkcs12_fill_buffer( data: *mut c_uchar, mut data_len: size_t,
+    filler: *const c_uchar, fill_len: size_t) {
 
 
-    println!("Hello, world!");
+    let mut p = data;
+    let mut use_len: size_t;
+
+    while data_len > 0 {
+        use_len = if data_len > fill_len { fill_len } else { data_len };
+        unsafe { memcpy( p as *mut c_void , filler as *mut c_void, use_len ) };
+        p = unsafe { p.offset(use_len as isize) }; //p += use_len;
+        data_len -= use_len;
+    }
+
     return;
 }
 
