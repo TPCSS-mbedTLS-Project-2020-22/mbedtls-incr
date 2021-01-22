ea5fa28f55fe8cf669dbe308311f9072ae0ca961
Alvin <alvingeorge>
2021-01-22 11:45:00 +0530

    lib.rs -- cleanup

--- mtlsrust/src/lib.rs
+++ mtlsrust/src/lib.rs
@@ -1,13 +1,10 @@
 
 
+
+// re-export symbols from pkcs12 module (ie pkcs12.rs file)
 pub mod pkcs12;
 pub use pkcs12::*;
 
-#[no_mangle]
-// pub unsafe extern "C" fn func_in_rust(x: i32) -> i32 {
-pub extern "C" fn func_in_rust(x: i32) -> i32 {
-        return x + 10;
-}
 
 
 #[cfg(test)]
